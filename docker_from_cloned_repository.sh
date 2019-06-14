69cb4346248f18da85d36a733b12238305c580ca
aspera-non-spernit <g.s.d>
2019-06-14 20:32:00 +0200

    add build script

---
+++ docker_from_cloned_repository.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -n "Do you want to run on cuda, cpu or opencl [cuda, cpu, opencl]?: "
+read env
+if [[ "$env" != "cuda" ]] || [[ "$env" != "cpu" ]] || [[ "$env" != "opencl" ]]; then
+    echo "$env is not a valid option. Choose from 'cuda', 'cpu' or 'opencl'"
+else
+    docker build --tag=aspera_non_spernit/deepfacelab -f docker/Dockerfile --build-arg requirements=$env .
+fi
