d66dee04de6146d2d10c02ba398febea73f29c88
aspera-non-spernit <g.s.d>
2019-07-22 13:21:44 +0200

    fix mkdir

--- docker_from_cloned_repository.sh
+++ docker_from_cloned_repository.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
 # Gathering installation information
-echo -n "Do you want to run on cuda, cpu or opencl [cuda, cpu, opencl]?: "
+echo -e -n "Do you want to run on cuda, cpu or opencl [cuda, cpu, opencl]?:\n"
 read env
 if [[ "$env" != "cuda" ]] && [[ "$env" != "cpu" ]] && [[ "$env" != "opencl" ]]; then
     echo "$env is not a valid option. Choose from 'cuda', 'cpu' or 'opencl'"
 elif [[ "$env" = "opencl" ]]; then
-    echo -n "What's your graphics card [mesa, nvidia, nvidia-390xx, ivybridge, haswell]?: "
+    echo -e -n "What's your graphics card [mesa, nvidia, nvidia-390xx, ivybridge, haswell]?:\n"
     read graphics
 fi
+
 # Installing docker
-docker build --tag=aspera_non_spernit/deepfacelab -f docker/Dockerfile --build-arg env=$env --build-arg graphics=$graphics .
-echo -e -n "Docker container with DeepFaceLab successfully installed."
+# docker build --tag=aspera_non_spernit/deepfacelab -f docker/Dockerfile --build-arg env=$env --build-arg graphics=$graphics .
+# echo -e -n "Docker container with DeepFaceLab successfully installed.\n"
+
 # Setting up workspace
-echo -e -n "Where do you want to have the workspace directory [default: /home/${USER}/DeepFaceLab/workspace)]:"
+echo -e -n "Where do you want to have the workspace directory [default: /home/${USER}/DeepFaceLab/workspace)]:\n"
 read workspace
 if [ "$workspace" = "" ]; then
     workspace=/home/${USER}/DeepFaceLab/workspace
 fi
-echo -n "Creating folder structure in workspace.."
+echo -e -n "Creating folder structure: $workspace\n"
 rm -rf $workspace
-mkdir -p $workspace
-mkdir $workspace/data_src
-mkdir $workspace/data_src/aligned
-mkdir $workspace/data_src/aligned_debug
-mkdir $workspace/data_dst
-mkdir $workspace/data_dst/aligned
-mkdir $workspace/data_dst/aligned_debug
-mkdir $workspace/model
+mkdir -p $workspace/{data_src/aligned, data_src/aligned_debug, data_dst/aligned, data_dst/aligned_debug, model}
+
 # Running or quitting installation
-echo -n "You can run and enter your Container with the command:"
-echo -n "docker run -ti -v /home/${USER}/workspace:/app/DeepFaceLab_Linux/workspace aspera_non_spernit/deepfacelab"
-echo -n "You can copy your video material into the workspace folder on the host machine."
-echo -n "DeepFaceLab_Docker will pick up changes."
+echo -e -n "You can run and enter your Container with the command:\n"
+echo -e -n "docker run -ti -v /home/${USER}/workspace:/app/DeepFaceLab_Linux/workspace aspera_non_spernit/deepfacelab\n"
+echo -e -n "You can copy your video material into the workspace folder on the host machine.\n"
+echo -e -n "DeepFaceLab_Docker will pick up changes.\n"
 echo -n "Run DeepFaceLab_Docker now? [y, n]?"
 read execute
-if [ "$execute" = "y"]; then
+if [ "$execute" = "y" ]; then
     docker run -ti -v /home/${USER}/workspace:/app/DeepFaceLab_Linux/workspace aspera_non_spernit/deepfacelab
 fi
-echo -n "Installation successful. Have fun. "
+echo -e -n "Installation successful. Have fun.\n"
+
