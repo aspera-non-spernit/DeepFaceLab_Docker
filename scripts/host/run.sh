8050eba5893bffbd1ff6b610fa4e30020762ed2d
aspera-non-spernit <g.s.d>
2019-07-22 18:09:25 +0200

    add docker run .... -v ..:rw

--- scripts/host/run.sh
+++ scripts/host/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti -v /home/${USER}/DeepFaceLab/workspace:/app/DeepFaceLab_Linux/workspace aspera_non_spernit/deepfacelab
+docker run -ti -v /home/${USER}/DeepFaceLab/workspace:/app/DeepFaceLab_Linux/workspace:rw aspera_non_spernit/deepfacelab
