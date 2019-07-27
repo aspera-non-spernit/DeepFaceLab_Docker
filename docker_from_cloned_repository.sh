9e226b460ed810ed9a331f0c46bac8dd11d5d9b5
aspera-non-spernit <g.s.d>
2019-07-27 14:37:21 +0200

    trying fix drivers

--- docker_from_cloned_repository.sh
+++ docker_from_cloned_repository.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 ENV="cuda"
 OPTIONAL="n"
-GRAPHICS=" "
+GRAPHICS="GTX1080"
 
 # If you run into trouble with CUDA not being available,
 # run nvidia-modprobe first.
 # You need to relogin in order for the cuda binaries to appear in your PATH
-
+# https://github.com/NVIDIA/nvidia-docker
+# scp arch@{}:/etc/ssh/sshd_conf ./docker/sshd_conf
 # quit on false user input.
 early_quit() {
     echo "Closing with error. Nothing has been installed."
     exit 1
 }
+
+# if on a blank arch installation execute arch_prep()
+arch_prep() {
+    sudo passwd
+    sudo pacman-keys --init
+    sudo pacman-key --populate archlinux
+    sudo cp docker/pacman.conf /etc/pacman.conf
+    
+    # sudo nano /etc/pacman.d/gnupg/gpg.conf
+    #>>keyserver hkp://pgp.mit.edu:11371
+
+    echo "127.0.0.1 localhost" >> /etc/hosts
+    
+    # todo: replace
+    echo 'DISPLAY="localhost:10.0"' >> etc/environment
+    sudo pacman -S linux-headers base-devel lib32-libxtst libsm libxrender python youtube-dl ffmpeg xorg-xinit xorg-xhost xorg-xauth xorg-xclock
+
+    sudo cp docker/sshd_conf /etc/ssh/sshd_conf
+}
+
 
 # Gathering installation information
 # for the environment and graphics card.
@@ -22,13 +43,34 @@ environment() {
     if [[ "$env" != "cuda" ]] && [[ "$env" != "cpu" ]] && [[ "$env" != "opencl" ]]; then
         echo "$env is not a valid option. Choose from: 'cuda', 'cpu' or 'opencl'"
         early_quit
+    # opencl ennvironment
     elif [[ "$env" = "opencl" ]]; then
         echo -e -n "What's your graphics card [mesa, nvidia, nvidia-390xx, ivybridge, haswell]?:\n"
-        read $graphics
-        GRAPHICS=$graphics
+        read graphics
+        GRAPHICS=$grapics
         if [[ "$graphics" != "mesa" ]] && [[ "$graphics" != "nvidia" ]] && [[ "$graphics" != "nvidia-390xx" ]] && [[ "$graphics" != "ivybridge" ]] && [[ "$graphics" != "haswell" ]]; then
-            echo "$env is not a valid option. Choose from: 'mesa', 'nvidia' 'nvidia-390xx', 'ivybridge' or 'haswell'"
+            echo "$graphics is not a valid option. Choose from: 'mesa', 'nvidia' 'nvidia-390xx', 'ivybridge' or 'haswell'"
             early_quit
+        fi
+	# cuda environment
+    elif [[ "$env" = "cuda" ]]; then
+        echo -e -n "What's your nvidia graphics card [GTX1080, V100]?:\n";
+        read graphics
+        if [[ "$graphics" != "GTX1080" ]] && [[ "$graphics" != "V100" ]]; then
+   			echo "$graphics is not a valid option. Choose from: 'GTX1080' or 'V100'"
+        	early_quit
+		fi
+		GRAPHICS=$graphics
+        if [[ "$graphics" = "V100" ]]; then
+            wget "http://us.download.nvidia.com/tesla/418.67/NVIDIA-Linux-x86_64-418.67.run";
+            chmod 700 ./NVIDIA-Linux-x86_64-418.67.run;
+            ./NVIDIA-Linux-x86_64-418.67.run;
+            rm ./NVIDIA-Linux-x86_64-418.67.run;
+        elif [[ "$graphics" = "GTX1080" ]]; then
+            wget "http://us.download.nvidia.com/XFree86/Linux-x86_64/430.34/NVIDIA-Linux-x86_64-430.34.run";
+            chmod 700 ./NVIDIA-Linux-x86_64-430.34.run;
+            ./NVIDIA-Linux-x86_64-430.34.run;
+            rm ./NVIDIA-Linux-x86_64-430.34.run;
         fi
     fi
 }
@@ -45,8 +87,13 @@ optional() {
 
 # Installing docker
 install() {
-    echo -e -n "Installing for a $env environment. Optional drivers: $graphics\n"
-    docker build --tag=aspera_non_spernit/deepfacelab -f docker/Dockerfile --build-arg env=$ENV --build-arg graphics=$GRAPHICS --build-arg=$OPTIONAL .
+    echo -e -n "Installing for a $env environment. "
+    if [[ "$env" != "cpu" ]]; then
+		echo -e -n "Optional drivers: $graphics\n"
+    else 
+        echo -e -n "\n"
+    fi
+  #  docker build --tag=aspera_non_spernit/deepfacelab -f docker/Dockerfile --build-arg env=$ENV --build-arg graphics=$GRAPHICS --build-arg=$OPTIONAL .
     echo -e -n "Docker container with DeepFaceLab successfully installed.\n"
 }
 
@@ -77,6 +124,10 @@ workspace() {
     cp ./scripts/host/run.sh $workspace/..
 }
 
+finish_installation() {
+    exec bash
+}
+
 # Running or quitting installation
 run() {
     echo -e -n "\nYou can run and enter your Container with the command:\n"
@@ -91,7 +142,9 @@ run() {
     echo -e -n "Installation successful. Have fun.\n"
 }
 environment
+#arch_prep
 #optional
 install
+finish_installation
 workspace
 run
